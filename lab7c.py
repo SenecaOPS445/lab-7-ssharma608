5a949869a806c40c73334ec720a9b88b759e22d2
Shikshya Sharma <ssharma608@myseneca.ca>
2025-03-21 15:39:23 -0400

    Added lab7a

--- lab7c.py
+++ lab7c.py
@@ -16,21 +16,8 @@ def format_time(t):
 
 def sum_times(t1, t2):
     """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
-    sum.hour = t1.hour + t2.hour
-    sum.minute = t1.minute + t2.minute
-    sum.second = t1.second + t2.second
-
-
-    if sum.second >= 60:
-        sum.minute = sum.minute + 1
-        sum.second = sum.second - 60
-
-    if sum.minute >= 60:
-        sum.hour = sum.hour + 1
-        sum.minute = sum.minute - 60
-
-    return sum
+    total_seconds = time_to_sec(t1) + time_to_sec(t2)
+    return sec_to_time(total_seconds)
 
 def valid_time(t):
     """check for the validity of the time object attributes:
@@ -40,3 +27,28 @@ def valid_time(t):
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
         return False
     return True
+
+
+def change_time(time, seconds):
+    new_time = time_to_sec(time) + seconds
+    updated_time = sec_to_time(new_time)
+    time.hour = updated_time.hour
+    time.minute = updated_time.minute
+    time.second = updated_time.second
+    return None
+
+def time_to_sec(time):
+    '''convert a time object to a single integer representing the number of seconds from mid-night'''
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def sec_to_time(seconds):
+    '''convert a given number of seconds to a time object in hour,minute,second format'''
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes,60)
+    return time
+
+
+
